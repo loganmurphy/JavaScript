847bd224251ce454be9c386a9111e3e87b510b54
loganmurphy <loganmurphy1984@gmail.com>
2017-10-18 09:09:43 -0500

    updated exercise_3

--- week2/closures/counter_3.js
+++ week2/closures/counter_3.js
@@ -10,6 +10,7 @@
 // > count.decrement()
 // 4
 
+// Here's the solution with a class
 class Count {
   constructor (count){
   this.counter = count;
@@ -34,3 +35,25 @@ count.increase();
 count.decrease();
 count.decrease();
 count.decrease();
+
+// heres the solution with closure
+function counter () {
+  var count = 0;
+  return {
+    increment: function () {
+      count++;
+      console.log(count);
+      return count;
+    },
+    decrement: function () {
+      count -= 1;
+      console.log(count);
+      return count;
+    }
+  }
+}
+
+c = counter();
+c.increment();
+c.increment();
+c.decrement();

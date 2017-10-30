ec18d00c008abd6caf2b2f1e01bcd2c11c5770f6
loganmurphy <loganmurphy1984@gmail.com>
2017-10-29 22:45:54 -0500

    web scraper

---
+++ week3/make_module/wiki_scrapie/README.txt
@@ -0,0 +1,6 @@
+This package is intended to be used to find information from wikipedia pages 
+by specifying the selector that you want to target. Both the complete html 
+page and and html page with all of the selector type will be saved to your 
+local hard drive in the ./scraped_info directory.
+
+Have fun!
---
+++ week3/make_module/wiki_scrapie/index.js
@@ -0,0 +1,52 @@
+// This is a freeform web scraping project. You will use request and cheerio to scrape a web site for information. Some ideas:
+//
+// Get information about countries, plants, animals from Wikipedia.
+// Get information about programming languages from Wikipedia.
+// Get information about Pokemon from...somewhere on the internet.
+// The end result of your project is a publish npm module, with which people can get information for a given thing or topic. For example, maybe they can get information about any programming language with this code:
+//
+// var programmingLanguages = require('programming-languages');
+// programmingLanguages.getInfo('JavaScript', function(err, info) {
+//   console.log(info); // And info will be an object containing various
+//                      // info on JavaScript on Wikipedia
+// });
+// You will design the API (application programming interface) - which is just a fancy word for how people will use your module. You will write a README file for your module, which will teach them how to use it.
+
+var fs = require('fs'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    readline = require('readline');
+    rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    search = ''
+    pageURL = 'https://Wikipedia.org/wiki/' + search;
+
+function scrapePage () {
+  rl.question("What do you want to search Wikipedia for? ", function(search) {
+    rl.question("What do you want to know about it? ", function(query) {
+      //make an HTTP request for the page to be scraped
+      request('https://Wikipedia.org/wiki/' + search, function(error, response, responseHtml){
+        if (error) {
+          console.error(error.message);
+          return;
+        }
+        //write the entire scraped page to the local file system
+        fs.writeFile('./scraped_info/' + search + '.html', responseHtml, function(err){
+            console.log('entire-page.html successfully written to HTML folder');
+            //write isolated sections of the entire scraped page to the local file system
+        });
+          //create the cheerio object
+          var $ = cheerio.load(responseHtml),
+            //create a reference to the desired element
+            $links = $(query).nextAll();
+        //write the data to the local file system
+        fs.writeFile('./scraped_info/' + search + '_' + query +'.html', $links, function(err){
+            console.log('title.html successfully written to HTML folder');
+        });
+      });
+    });
+  });
+}
+scrapePage();
